a996498b3aa4a907de63abfbe7506369d9f8679d
Travis Palmer <palmer.travis@gmail.com>
2016-10-21 14:33:21 -0400

    initial project commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+ruby File.read(File.join(File.dirname(__FILE__), '.ruby-version')).strip
+
+gem 'cocoapods'
---
+++ Pods/Target Support Files/Pods-BoosterKitUITests/Pods-BoosterKitUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BoosterKitUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BoosterKitUITests
+@end
