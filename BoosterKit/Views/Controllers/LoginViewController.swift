8203db6767d1fc486d01e771d7ab7a041399ec06
Travis Palmer <palmer.travis@gmail.com>
2017-01-13 15:51:24 -0500

    create LoginViewController

---
+++ BoosterKit/Views/Controllers/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  BoosterKit
+//
+//  Created by Travis Palmer on 1/13/17.
+//  Copyright © 2017 Spartan. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+}
